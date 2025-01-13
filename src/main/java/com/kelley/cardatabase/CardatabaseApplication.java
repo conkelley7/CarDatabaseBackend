39eaae1eae61e86da93380b5f1a4311d3e4dcace
conke <conke@10.0.0.241>
2025-01-13 14:08:33 -0600

    Overriden run method now adds Owners sample data to database

--- src/main/java/com/kelley/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/kelley/cardatabase/CardatabaseApplication.java
@@ -5,7 +5,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.kelley.cardatabase.domain.model.Car;
+import com.kelley.cardatabase.domain.model.Owner;
 import com.kelley.cardatabase.domain.repository.CarRepository;
+import com.kelley.cardatabase.domain.repository.OwnerRepository;
+
+import java.util.Arrays;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -19,9 +23,11 @@ public class CardatabaseApplication implements CommandLineRunner {
 	private static final Logger logger = LoggerFactory.getLogger(CardatabaseApplication.class);
 	
 	private final CarRepository carRepository;
+	private final OwnerRepository ownerRepository;
 	
-	public CardatabaseApplication(CarRepository carRepository) {
+	public CardatabaseApplication(CarRepository carRepository, OwnerRepository ownerRepository) {
 		this.carRepository = carRepository;
+		this.ownerRepository = ownerRepository;
 	}
 	
 	public static void main(String[] args) {
@@ -34,9 +40,16 @@ public class CardatabaseApplication implements CommandLineRunner {
 	 */
 	@Override
 	public void run(String... args) throws Exception{
-		// Saving two sample cars to H2 database.
-		carRepository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2023, 59000));
-		carRepository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2020, 29000));
+		// Add owners to H2 database
+		Owner owner1 = new Owner("John", "Doe");
+		Owner owner2 = new Owner("Jane", "Doe");
+		
+		ownerRepository.saveAll(Arrays.asList(owner1, owner2));
+		
+		// Add car objects and assign to owners in database
+		carRepository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2023, 59000, owner1));
+		carRepository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2020, 29000, owner2));
+		carRepository.save(new Car("Jeep", "Patriot", "Navy", "CPK-5561", 2016, 4200, owner2));
 		
 		// Fetching all cars just added and logging them in console
 		for (Car car : carRepository.findAll()) {
