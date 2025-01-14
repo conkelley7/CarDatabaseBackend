7d0fe6a7134abcbe0edbe93cfc46740073aaa091
conke <conke@10.0.0.241>
2025-01-14 12:12:10 -0600

    Added additional search methods to CarRepository

--- src/main/java/com/kelley/cardatabase/domain/repository/CarRepository.java
+++ src/main/java/com/kelley/cardatabase/domain/repository/CarRepository.java
@@ -1,11 +1,22 @@
 package com.kelley.cardatabase.domain.repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.kelley.cardatabase.domain.model.Car;
 
 /**
  * Repository for 'Car' Entity class.
  */
+@RepositoryRestResource
 public interface CarRepository extends CrudRepository<Car, Long> {
+	
+	// Fetch cars by brand
+	List<Car> findByBrand(@Param("brand") String brand);
+	
+	// Fetch cars by color
+	List<Car> findByColor(@Param("color") String color);
 }
