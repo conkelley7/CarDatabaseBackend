dcef3457abe04e3ce69536299c6bd3bf7260c044
conke <conke@10.0.0.241>
2025-01-19 12:53:52 -0600

    Created AuthenticationFilter.java
    AuthenticationFilter is used to authenticate all requests that require
    authentification (all requests besides /login). AuthenticationFilter was
    added to SecurityConfig as a dependency for use in SecurityFilterChain
    bean.

--- src/main/java/com/kelley/cardatabase/config/SecurityConfig.java
+++ src/main/java/com/kelley/cardatabase/config/SecurityConfig.java
@@ -13,15 +13,20 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.kelley.cardatabase.security.AuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 	
 	private final UserDetailsService userDetailsService;
+	private final AuthenticationFilter authenticationFilter;
 	
-	public SecurityConfig(UserDetailsService userDetailsService) {
+	public SecurityConfig(UserDetailsService userDetailsService, AuthenticationFilter authenticationFilter) {
 		this.userDetailsService = userDetailsService;
+		this.authenticationFilter = authenticationFilter;
 	}
 	
 	/**
@@ -47,7 +52,8 @@ public class SecurityConfig {
 		http.csrf((csrf) -> csrf.disable())
 			.sessionManagement((sessionManagement) -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests.requestMatchers(HttpMethod.POST, "/login").permitAll()
-			.anyRequest().authenticated());
+			.anyRequest().authenticated())
+			.addFilterBefore(authenticationFilter, UsernamePasswordAuthenticationFilter.class);
 			
 		return http.build();
 	}
