64fb4be5f3595923d4a23347cdc7e7200ac6ed0d
conke <conke@10.0.0.241>
2025-01-19 15:50:53 -0600

    Added CORS filter to security configuration class.

--- src/main/java/com/kelley/cardatabase/config/SecurityConfig.java
+++ src/main/java/com/kelley/cardatabase/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.kelley.cardatabase.config;
 
+import java.util.Arrays;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -14,6 +16,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 import com.kelley.cardatabase.security.AuthEntryPoint;
 import com.kelley.cardatabase.security.AuthenticationFilter;
@@ -45,6 +52,23 @@ public class SecurityConfig {
 		return new BCryptPasswordEncoder();
 	}
 	
+	/*
+	 *  Allowing all origins' http methods and headers for the time being, as indicated by (*) 
+	 */
+	@Bean
+	public CorsConfigurationSource corsConfigurationSource() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration();
+		config.setAllowedOrigins(Arrays.asList("*"));
+		config.setAllowedMethods(Arrays.asList("*"));
+		config.setAllowedHeaders(Arrays.asList("*"));
+		config.setAllowCredentials(false);
+		config.applyPermitDefaultValues();
+		
+		source.registerCorsConfiguration("/**", config);
+		return source;
+	}
+	
 	@Bean
 	public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
 		return authConfig.getAuthenticationManager();
@@ -53,6 +77,7 @@ public class SecurityConfig {
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http.csrf((csrf) -> csrf.disable())
+			.cors(withDefaults())
 			.sessionManagement((sessionManagement) -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
 			.authorizeHttpRequests((authorizeHttpRequests) -> authorizeHttpRequests.requestMatchers(HttpMethod.POST, "/login").permitAll()
 			.anyRequest().authenticated())
