be4d9fcb1b36b259af2ab8cbca38fd2921b94410
conke <conke@10.0.0.241>
2025-01-19 18:53:42 -0600

    Created Unit Test class for Owner Repository.

--- src/main/java/com/kelley/cardatabase/domain/repository/OwnerRepository.java
+++ src/main/java/com/kelley/cardatabase/domain/repository/OwnerRepository.java
@@ -1,8 +1,11 @@
 package com.kelley.cardatabase.domain.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 
 import com.kelley.cardatabase.domain.model.Owner;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+	Optional<Owner> findByFirstName(String firstName);
 }
---
+++ src/test/java/com/kelley/cardatabase/OwnerRepositoryTest.java
@@ -0,0 +1,29 @@
+package com.kelley.cardatabase;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import com.kelley.cardatabase.domain.model.Owner;
+import com.kelley.cardatabase.domain.repository.OwnerRepository;
+
+@DataJpaTest
+public class OwnerRepositoryTest {
+	@Autowired
+	private OwnerRepository ownerRepository;
+	
+	@Test
+	void saveOwner() {
+		ownerRepository.save(new Owner("Lucy", "Smith"));
+		assertThat(ownerRepository.findByFirstName("Lucy").isPresent()).isTrue();
+	}
+	
+	@Test
+	void deleteOwners() {
+		ownerRepository.save(new Owner("Lisa", "Morrison"));
+		ownerRepository.deleteAll();
+		assertThat(ownerRepository.count()).isEqualTo(0);
+	}
+}
