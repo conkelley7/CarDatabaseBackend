432ae88910a8ef3d11135bf894e11cb9e30431c0
conke <conke@10.0.0.241>
2025-01-24 22:05:27 -0600

    Added Additional GET endpoints
    Added an endpoint for getting cars by brand
    Added an endpoint for getting cars by color

--- src/main/java/com/kelley/cardatabase/domain/controller/CarController.java
+++ src/main/java/com/kelley/cardatabase/domain/controller/CarController.java
@@ -1,25 +1,42 @@
 package com.kelley.cardatabase.domain.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kelley.cardatabase.domain.model.Car;
-import com.kelley.cardatabase.domain.repository.CarRepository;
+import com.kelley.cardatabase.domain.service.CarService;
 
 @RestController
 public class CarController {
 	
-	private final CarRepository carRepository;
-	
-	public CarController(CarRepository carRepository) {
-		this.carRepository = carRepository;
-	}
-	
-	/**
-	 * Fetch and return all cars
-	 */
-	@GetMapping("/cars")
-	public Iterable<Car> getCars() {
-		return carRepository.findAll();
-	}
+    private final CarService carService;
+
+    public CarController(CarService carService) {
+        this.carService = carService;
+    }
+
+    /**
+     * Fetch and return all cars
+     */
+    @GetMapping("/cars")
+    public Iterable<Car> getCars() {
+        return carService.getAllCars();
+    }
+
+    /**
+     * Get cars by brand
+     */
+    @GetMapping("/cars/brand/{brand}")
+    public Iterable<Car> getCarByBrand(@PathVariable("brand") String brand) {
+        return carService.getCarsByBrand(brand);
+    }
+
+    /**
+     * Get cars by color
+     */
+    @GetMapping("/cars/color/{color}")
+    public Iterable<Car> getCarByColor(@PathVariable("color") String color) {
+        return carService.getCarsByColor(color);
+    }
 }
