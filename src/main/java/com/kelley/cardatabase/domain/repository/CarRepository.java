064c0583be519df9ad97b14127f1c1e9d95ef5a5
conke <conke@10.0.0.241>
2025-01-24 22:07:01 -0600

    Improved documentation for Repository methods

--- src/main/java/com/kelley/cardatabase/domain/repository/CarRepository.java
+++ src/main/java/com/kelley/cardatabase/domain/repository/CarRepository.java
@@ -1,6 +1,8 @@
 package com.kelley.cardatabase.domain.repository;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -13,9 +15,17 @@ import com.kelley.cardatabase.domain.model.Car;
 @RepositoryRestResource
 public interface CarRepository extends CrudRepository<Car, Long> {
 	
-	// Fetch cars by brand
-	List<Car> findByBrand(@Param("brand") String brand);
+	/**
+	 *  Fetch cars by brand
+	 * @param brand
+	 * @return List of Cars matching brand
+	 */
+	List<Car> findByBrand(String brand);
 	
-	// Fetch cars by color
-	List<Car> findByColor(@Param("color") String color);
+	/**
+	 * 
+	 * @param color
+	 * @return List of Cars matching color
+	 */
+	List<Car> findByColor(String color);
 }
--- src/main/java/com/kelley/cardatabase/domain/repository/OwnerRepository.java
+++ src/main/java/com/kelley/cardatabase/domain/repository/OwnerRepository.java
@@ -7,5 +7,10 @@ import org.springframework.data.repository.CrudRepository;
 import com.kelley.cardatabase.domain.model.Owner;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+	/**
+	 * Find an owner by their first name.
+	 * @param firstName
+	 * @return Owner (Optional) matching First Name
+	 */
 	Optional<Owner> findByFirstName(String firstName);
 }
