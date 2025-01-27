88872fc4d52e5d79da3919db87585e88e2c9675a
conke <conke@10.0.0.241>
2025-01-27 12:45:17 -0600

    Added new findByLastName method to repository.

--- src/main/java/com/kelley/cardatabase/domain/repository/OwnerRepository.java
+++ src/main/java/com/kelley/cardatabase/domain/repository/OwnerRepository.java
@@ -1,16 +1,26 @@
 package com.kelley.cardatabase.domain.repository;
 
-import java.util.Optional;
+import java.util.List;
 
 import org.springframework.data.repository.CrudRepository;
 
 import com.kelley.cardatabase.domain.model.Owner;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+	
+	
+	
 	/**
 	 * Find an owner by their first name.
 	 * @param firstName
-	 * @return Owner (Optional) matching First Name
+	 * @return List of owners matching the last name.
 	 */
-	Optional<Owner> findByFirstName(String firstName);
+	List<Owner> findByFirstName(String firstName);
+	
+	/**
+	 * 
+	 * @param lastName
+	 * @return List of owners matching the last name.
+	 */
+	List<Owner> findByLastName(String lastName);
 }
